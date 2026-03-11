ab83887ec6a635f615f8913dd9e414c313691a08
Aditya Saxena <137895529+adityaaa2511@users.noreply.github.com>
2026-03-11 10:37:54 +0530

    Added code for train method

--- partb/bpe_tokenizer.py
+++ partb/bpe_tokenizer.py
@@ -30,7 +30,59 @@ class BPETokenizer:
         return new_splits
 
     def train(self, corpus):
-        raise NotImplementedError("Training method not implemented yet.")
+        if isinstance(corpus, list):
+            corpus = "\n".join(corpus)
+
+        word_freqs = defaultdict(int)
+        words = re.findall(r"\S+", corpus)
+        for word in words:
+            word_freqs[word + "</w>"] += 1 # Add </w> to indicate end of word for now
+
+        splits = {}
+        for word, freq in word_freqs.items():
+            word_without_end = word[:-4]
+            chars = list(word_without_end) + ["</w>"]
+            splits[" ".join(chars)] = freq  # Add delimiter between characters and end of word
+
+        char_vocab = set()
+        for word in splits:
+            for ch in word.split():
+                char_vocab.add(ch)
+
+        current_vocab_size = len(self.special_tokens) + len(char_vocab)
+
+        self.merges = []
+        while current_vocab_size < self.target_vocab_size:
+            stats = self.get_stats(splits)
+            if not stats:
+                break
+
+            best_pair = max(stats, key=stats.get)
+            if stats[best_pair] < 1:
+                break
+            splits = self.merge_pair(best_pair, splits)
+            self.merges.append(best_pair)
+            current_vocab_size += 1
+
+        # Build the final vocabulary
+        self.vocab = {}
+        idx = 0
+        for token in self.special_tokens:
+            self.vocab[token] = idx
+            idx += 1
+
+        for ch in sorted(char_vocab):
+            if ch not in self.vocab:
+                self.vocab[ch] = idx
+                idx += 1
+
+        for pair_a, pair_b in self.merges:
+            merged = pair_a + pair_b
+            if merged not in self.vocab:
+                self.vocab[merged] = idx
+                idx += 1
+
+        self.inverse_vocab = {v: k for k, v in self.vocab.items()}
 
     def encode(self, text):
         raise NotImplementedError("Encoding method not implemented yet.")
