e11e389e18d95bf932bbe61251d3c9fa880a6b1d
Aditya Saxena <137895529+adityaaa2511@users.noreply.github.com>
2026-03-21 02:08:13 +0530

    num_layers->n_layers

--- partc/train_model.py
+++ partc/train_model.py
@@ -132,7 +132,7 @@ def main(args):
     config = {
         "d_model": 512,
         "n_heads": 8,
-        "num_layers": 6,
+        "n_layers": 6,
         "d_head": 64,
         "d_ff": 2048,
         "dropout": 0.1,
